bad5279607fc93d8aafd06f9697340cc29e278d5
Refaster24 <refaster24@gmail.com>
2023-08-07 03:42:30 +0300

    ediet

--- webpush.js
+++ webpush.js
@@ -15,11 +15,13 @@ webpush.setVapidDetails(
 
 // This is the same output of calling JSON.stringify on a PushSubscription
 const pushSubscription = {
-  endpoint: '.....',
+  endpoint: "https://fcm.googleapis.com/fcm/send/e8J7qD7Ww1c:APA91bH2ydu5tTDSVKH9Gul9w6l-RQr0WNJR7TRz1mEQ11Tc1nhniB60MUt1xFTBBS7F4jpHHxYmePdoG2SAUOwV53a-gTr8tWTyeVoKIWZ8DzF7V0AAHOnBL3Eti8bQtPXijnpTw1te",
+  expirationTime: null,
   keys: {
-    auth: '.....',
-    p256dh: '.....'
-  }
+    p256dh: "BLc-LRAexFgBbSSc-T1uNQ22XjcgEKX0b35xnMh6z0E1ibSPFmr0f5OImnbEJKusC2nnasWpT33C6saivi1-t7k",
+    auth: "_lPcGzkzqMPgos7ccghiGg",
+  },
 };
+
 
 webpush.sendNotification(pushSubscription, 'Your Push Payload Text');
